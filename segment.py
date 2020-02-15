b2dcf344cf21497954feb2d1368869c646308439
David Hunt <davhunt@indiana.edu>
2020-02-15 00:30:41 -0500

    first commit

--- segment.py
+++ segment.py
@@ -6,7 +6,7 @@ import nibabel as nib
 from DARTS import Segmentation
 
 #seg_obj = Segmentation(model_wts_path='./saved_model_wts/dense_unet_saggital_finetuned.pth', model_type='dense-unet', use_gpu=False)
-seg_obj = Segmentation(model_wts_path='/N/u/davhunt/Carbonate/app-darts/saved_model_wts/dense_unet_saggital_finetuned.pth', model_type='dense-unet', use_gpu=False)
+seg_obj = Segmentation(model_wts_path='/N/u/davhunt/Carbonate/app-darts/saved_model_wts/dense_unet_back2front_finetuned.pth', model_type='dense-unet', use_gpu=False)
 seg_out, seg_proba_out = seg_obj.predict(inputs=sys.argv[1])
 
 nib.save(seg_out,'parc/parc.nii.gz')
