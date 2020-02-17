1344ba9d078a5166bb7be4af5bd810da8c1efe26
David Hunt <davhunt@indiana.edu>
2020-02-17 10:12:51 -0500

    save niftis

--- segment.py
+++ segment.py
@@ -9,5 +9,8 @@ from DARTS import Segmentation
 seg_obj = Segmentation(model_wts_path='/N/u/davhunt/Carbonate/app-darts/saved_model_wts/dense_unet_back2front_finetuned.pth', model_type='dense-unet', use_gpu=False)
 seg_out, seg_proba_out = seg_obj.predict(inputs=sys.argv[1])
 
-nib.save(seg_out,'parc/parc.nii.gz')
-nib.save(seg_proba_out, 'softmax_prob_map.nii.gz')
+T1 = nib.load(sys.argv[1])
+parc_img = nib.Nifti1Image(seg_out, T1.affine)
+prob_map_img = nib.Nifti1Image(se_proba_out, T1.affine)
+nib.save(parc_img,'parc/parc.nii.gz')
+nib.save(prob_map_img, 'softmax_prob_map.nii.gz')
