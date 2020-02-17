ce1660148c4b9a3681645e1ca18cf2a63fcb9c1e
David Hunt <davhunt@indiana.edu>
2020-02-17 10:46:29 -0500

    save niftis

--- segment.py
+++ segment.py
@@ -11,6 +11,6 @@ seg_out, seg_proba_out = seg_obj.predict(inputs=sys.argv[1])
 
 T1 = nib.load(sys.argv[1])
 parc_img = nib.Nifti1Image(seg_out, T1.affine)
-prob_map_img = nib.Nifti1Image(se_proba_out, T1.affine)
+prob_map_img = nib.Nifti1Image(seg_proba_out, T1.affine)
 nib.save(parc_img,'parc/parc.nii.gz')
 nib.save(prob_map_img, 'softmax_prob_map.nii.gz')
